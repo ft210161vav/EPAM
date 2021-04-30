aae855c86275bdf3e3140f4c97190bdd491402d8
Алексей Винарский <a.vinarsky@gmail.com>
2021-04-30 16:25:34 +0300

    FiltersCategoriesTest formatted

--- src/test/java/FiltersCategoriesTest.java
+++ src/test/java/FiltersCategoriesTest.java
@@ -25,19 +25,25 @@ public class FiltersCategoriesTest {
         startPage.init();
         //* 1 Пользователь переходит на вкладку Talks Library
         startPage.videoTalks();
+
         //* 2 Пользователь нажимает на More Filters
         $(".evnt-toggle-filters-button.evnt-button.btn").shouldBe(Condition.visible).click();
+
         //* 3 Пользователь выбирает: Category – Testing
         $(By.id("filter_category")).should(Condition.visible).click();
         $("[data-group='Testing']").scrollIntoView(true).shouldBe(Condition.visible).click();
+
         //* Location – Belarus
         $(By.id("filter_location")).should(Condition.visible).click();
         $("[data-group='Belarus']").scrollIntoView(false).shouldBe(Condition.visible).click();
+
         // Language – English
         $(By.id("filter_language")).should(Condition.visible).click();
         $("[data-value='ENGLISH']").shouldBe(Condition.visible).click();
+
         //Скрываем фильтры
         $(".evnt-toogle-filters-text.hide-more").shouldBe(Condition.visible).click();
+
         //     * 4 На странице отображаются карточки соответствующие правилам выбранных фильтров
         assert ($$(".evnt-talk-card").size()>0);
         String lang=$(".language").getText().substring(0,2);
